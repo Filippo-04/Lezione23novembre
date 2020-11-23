cc779679171e181c384d651cd7d4ddc03837ccba
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 08:48:36 +0100

    Prima modifica

--- Lezione23novembre/Program.cs
+++ Lezione23novembre/Program.cs
@@ -6,7 +6,7 @@ namespace Lezione23novembre
         static void Main(string[] args)
         {
             string unicodeString =
-                "This Unicode string has 2 characters outside the" +
+                "This Unicode string has 2 characters outside  the" +
                 "ASCII range: \n" +
                 "PI (\u03A0), and Sigma (\u03A0).";
             Console.WriteLine("Original String:");
