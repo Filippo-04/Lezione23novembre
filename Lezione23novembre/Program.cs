b26e82465c6b5da169f254aa29e3aca476a4ea2f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-23 08:42:15 +0100

    Add project files.

---
+++ Lezione23novembre/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Lezione23novembre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unicodeString =
+                "This Unicode string has 2 characters outside the" +
+                "ASCII range: \n" +
+                "PI (\u03A0), and Sigma (\u03A0).";
+            Console.WriteLine("Original String:");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodeString);
+            Console.WriteLine();
+        }
+    }
+}
